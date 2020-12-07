7dd76cf391864b677f62582a351c85f543566059
arget4me <32455602+arget4me@users.noreply.github.com>
2020-12-07 16:04:40 +0100

    3) phase and magnitude

--- Lab2/lab2.m
+++ Lab2/lab2.m
@@ -107,14 +107,25 @@ imshow(img_out, []); title('Laplacian', 'Interpreter', 'none');
 %% 2 Bilateral Filtering
 clear all; close all; clc;
 
-sigma_range = [2,  6, 18];
+sigma_range = [2,  6, 18]; 
+%@Note: if range_sigma is larger than the kernel size it will have little
+%affect due to the normalization factor, Wp, rescaling the values used. In
+%other words the weights in the kernel is rescaled to sum up to 1. If
+%range_sigma is larger then the kernel size then the assumption is that in 
+%just one standardeviation the kernel_size is exceeded and can't affect the result. 
+%What will tend to happen with a much larger range_sigma then the kernel_size is 
+%that only a small part around the mean is used and results in a filter similar to the 
+%mean filter instead of a gaussian (For range only. the intensity is a gaussian 
+%with sigma_domain as standard deviation)
+
 sigma_domain = [0.1, 0.25, 10];
-kernel_size = 18;
+kernel_size = 9;
 
-subplot(1, 2, 1); boat = im2double(imread("boat.tif")); imshow(boat); title("Boat Original");
-subplot(1, 2, 2); clown = im2double(imread("clown.tif")); imshow(clown); title("Clown Original");
+subplot(1, 3, 1); boat = im2double(imread("boat.tif")); imshow(boat); title("Boat Original");
+subplot(1, 3, 2); clown = im2double(imread("clown.tif")); imshow(clown); title("Clown Original");
+subplot(1, 3, 3); mandrill = im2double(imread("mandrill.tif")); imshow(mandrill); title("Mandrill Original");
 
-boats = figure; clowns = figure;
+boats = figure; clowns = figure; mandrills = figure;
 for r = 1:length(sigma_range)
     for d = 1:length(sigma_domain)
         
@@ -127,18 +138,113 @@ for r = 1:length(sigma_range)
         %apply bilateral filtering to clown image
         filtered_clown = bilateralFiltering(clown, sigma_range(r), sigma_domain(d), kernel_size); imshow(filtered_clown);
         title(['\sigma_{s} = ', num2str(sigma_range(r)), ' | \sigma_{r} = ', num2str(sigma_domain(d))]);
+        
+        figure(mandrills); subplot(length(sigma_range), length(sigma_domain), d + (r-1) * length(sigma_domain));
+        %apply bilateral filtering to mandrill image
+        filtered_mandrill = bilateralFiltering(mandrill, sigma_range(r), sigma_domain(d), kernel_size); imshow(filtered_mandrill);
+        title(['\sigma_{s} = ', num2str(sigma_range(r)), ' | \sigma_{r} = ', num2str(sigma_domain(d))]);
     end
 end
 
 %% 3 Fourier Transform
-%%   3.1 Phase and Magnitude
+%{
+    @Note:
+    I feel that 3.1 and 3.2 are very closely related. I have implemented
+    the in the same section and present the result from the at the same
+    time.
+%}
+
+
+%  3.1 Phase and Magnitude -----------------------------------------------------------------------------------------------------------
+
 clear all; close all; clc;
 
+mandrill = im2double(imread("mandrill.tif")); mandrill = rgb2gray(mandrill);
+
+mandrill_fft = fftshift(fft2(mandrill));
+mandrill_magnitude_matlab = abs(mandrill_fft);
+mandrill_phase_matlab = angle(mandrill_fft);
 
 
-%%    3.2 Phase vs Magnitude
-clear all; close all; clc;
+%  3.2 Phase vs Magnitude -------------------------------------------------------------------------------------------------------------
+%generate a random complex number
+z1 = (2 * rand() - 1) + (2 * rand() * i - i);
 
+%magnitude: Matlab function abs(complex_number)
+%abs(complex_number) = sqrt(Re(complex_number)^2 + Im(complex_number)^2)
+magnitude = @(complex_number) sqrt(real(complex_number).^2 + imag(complex_number).^2)
+r = magnitude(z1);
+diff_abs = r - abs(z1);
+
+%phase angle: Matlab function angle(complex_number)
+%angle(complex_number) = atan(Im(complex_number) / Re(complex_number))
+phase = @(complex_number) atan2(imag(complex_number), real(complex_number))
+theta = phase(z1);
+diff_angle = theta - angle(z1);
+
+
+%eulers formula exp(-j * phi) = cos(phi) + i*sin(phi)
+%using magnitude and phase
+%complex_number = magnitude * (cos(phase) + i * sin(phase))
+complex = @(magnitude, phase) magnitude .* (cos(phase) + i .*sin(phase))
+z2 = complex(r, theta);
+diff_complex = magnitude(z2 - z1);
+
+fprintf("Complex number z = %f + %fi\n", real(z1), imag(z1));
+fprintf("r = magnitude(z1) = %f. Difference between r and MATLAB function abs(z1) = %f\n", r, diff_abs);
+fprintf("theta = phase(z1) = %f. Difference between theta and MATLAB function angle(z1) = %f\n", theta, diff_angle);
+fprintf("z2 = complex(r, theta) = %f + %fi. Difference between z2 and original z1 = %f\n", real(z2), imag(z2), diff_complex);
+
+% calculate magnitude and phase for "mandrill.tif" with formulas
+mandrill_magnitude = magnitude(mandrill_fft);
+mandrill_phase = phase(mandrill_fft);
+
+%load clown image and calculate phase. 
+clown = im2double(imread("clown.tif"));
+clown_fft = fftshift(fft2(clown));
+clown_magnitude = magnitude(clown_fft);
+clown_phase = phase(clown_fft);
+
+%Switch phase and magnitude between "clown.tif" and "mandril.tif" and
+%perform inverse fourier transform
+replaced_phase = complex(mandrill_magnitude, clown_phase);
+replaced_phase_result = real(ifft2(fftshift(replaced_phase)));
+
+replaced_magnitude = complex(clown_magnitude, mandrill_phase);
+replaced_magnitude_result = real(ifft2(fftshift(replaced_magnitude)));
+
+%Calculate magnitude and phase for the new images
+replaced_phase_fft = fftshift(fft2(replaced_phase_result));
+replaced_magnitude_fft = fftshift(fft2(replaced_magnitude_result));
+
+replaced_phase_fft_magnitude = magnitude(replaced_phase_fft);
+replaced_phase_fft_phase = phase(replaced_phase_fft);
+replaced_magnitude_fft_magnitude = magnitude(replaced_magnitude_fft);
+replaced_magnitude_fft_phase = phase(replaced_magnitude_fft);
+
+%3.2 and 3.2 Resulting plots ----------------------------------------------------------------------------------------------------------
+figure("name", "Mandrill & Clown spatial domain");
+subplot(2, 2, 1); imagesc(mandrill); colormap(gray); title("Mandrill");
+subplot(2, 2, 2); imagesc(clown); colormap(gray); title("Clown");
+subplot(2,2,3); imagesc(replaced_phase_result); colormap(gray); title("Mandrill magnitude & Clown phase");
+subplot(2,2,4); imagesc(replaced_magnitude_result); colormap(gray); title("Clown magnitude & Mandrill phase");
+
+
+figure("name", "Mandrill & Clown frequency domain");
+subplot(2, 3, 1); imagesc(log(1 + abs(mandrill_magnitude_matlab))); colormap(gray); title("Mandrill Magnitude - MATLAB");
+subplot(2, 3, 4); imagesc(mandrill_phase_matlab); colormap(gray); title("Mandrill Phase - MATLAB");
+
+subplot(2, 3, 2); imagesc(log(1 + mandrill_magnitude)); colormap(gray); title("Mandrill Magnitude - formula");
+subplot(2, 3, 5); imagesc(mandrill_phase); colormap(gray); title("Mandrill Phase - formula");
+
+subplot(2, 3, 3); imagesc(log(1 + clown_magnitude)); colormap(gray); title("Clown Magnitude");
+subplot(2, 3, 6); imagesc(clown_phase); colormap(gray); title("Clown Phase");
+
+figure("name", "Swapped Mandrill & Clown frequency domain");
+subplot(2, 2, 1); imagesc(log(1 + replaced_phase_fft_magnitude)); colormap(gray); title("Mandrill magnitude & Clown phase : Magnitude");
+subplot(2, 2, 3); imagesc(replaced_phase_fft_phase); colormap(gray); title("Mandrill magnitude & Clown phase : Phase");
+subplot(2,2,2); imagesc(log(1 + replaced_magnitude_fft_magnitude)); colormap(gray); title("Clown magnitude & Mandrill phase : Magnitude");
+subplot(2,2,4); imagesc(replaced_magnitude_fft_phase); colormap(gray); title("Clown magnitude & Mandrill phase : Phase");
 
 
 %% 4 Filtering in the frequency domain 
