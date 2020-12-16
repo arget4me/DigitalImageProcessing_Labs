f30c4517504dccdb3d1da651d173df1a0e255aca
arget4me <32455602+arget4me@users.noreply.github.com>
2020-12-16 17:57:36 +0100

    Update lab3.m

--- Lab3/lab3.m
+++ Lab3/lab3.m
@@ -155,8 +155,121 @@ for i = 1:length(query_images)
 end
 %% 2 Detection, segmentation and object statistics
 %% 2.1 Scientific background and data
+clear all; close all; clc;
+
+%@QUESTION: Change structure inside matlab, or manually in folder??
+
+cd images/Oats
+    addpath('Gotala 50 corns')
+    addpath('Lanna 50 corns')
+    addpath('Multorp 50 corns')
+
+    farms = {'Gotala', 'Lanna', 'Multorp'};
+    all_images{1} = dir([farms{1} ' 50 corns/*.jpg']);
+    all_images{2} = dir([farms{2} ' 50 corns/*.jpg']);
+    all_images{3} = dir([farms{3} ' 50 corns/*.jpg']);
+cd ../..
+
+number_of_images = length(all_images{1}) + length(all_images{2}) +length(all_images{3});
+
+%Collection row layout  {Image, Farm, Cultivation, ShootStage
+cultivations = {'Belinda', 'Fatima', 'Symphony', 'Unkown'};
+cultivations_search_words = [" bel ", " fat ", " symp "];
+shoots = {'Huvudskott', 'Gronskott', 'Unkown'};
+shoots_search_words = [" huv ", " grn "];
+
+collection{number_of_images, 2} = [];
+start_index_offset = 0;
+for i = 1:length(all_images)
+    for k = 1:length(all_images{i})
+        current_index = start_index_offset + k;
+        
+        s = struct("file_name", '', "farm_index", 0, "cultivation_index", 0, "shoot_index", 0);
+        s.file_name = all_images{i}(k).name;
+        s.farm_index = i;
+        s.cultivation_index = length(cultivations_search_words) + 1;
+        s.shoot_index = length(shoots_search_words) + 1;
+        
+        for cultivation = 1:length(cultivations_search_words)
+            if contains(lower(all_images{i}(k).name), cultivations_search_words(cultivation))
+                s.cultivation_index = cultivation;
+                break;
+            end
+        end
+        
+        for shoot = 1:length(shoots_search_words)
+            if contains(lower(all_images{i}(k).name), shoots_search_words(shoot))
+                s.shoot_index = shoot;
+                break;
+            end
+        end
+        
+        collection{current_index, 1} = imrotate(imread(s.file_name), -90);
+        collection{current_index, 2} = s;
+        fprintf("'%s':\t%s\t%s\t%s\n", collection{current_index, 2}.file_name, farms{collection{current_index, 2}.farm_index}, cultivations{collection{current_index, 2}.cultivation_index}, shoots{collection{current_index, 2}.shoot_index});
+        
+    end
+    
+    start_index_offset = start_index_offset + length(all_images{i});
+end
+
 
 %% 2.2 Segmentation
+
+index = randi(length(collection(:, 1)), 4);
+%{
+figure('name', 'Laplacian');
+for i = 1:4
+    img = imfilter(rgb2gray(collection{index(i), 1}), fspecial("laplacian"));%collection{index(i), 1}, fspecial("laplcian"))
+    title_string = [farms{collection{index(i), 2}.farm_index},' ', cultivations{collection{index(i), 2}.cultivation_index},' ',shoots{collection{index(i), 2}.shoot_index}];
+    subplot(2, 2, i); imshow(img); title(title_string);
+end
+
+figure('name', 'Sobel');
+for i = 1:4
+    img = imfilter(rgb2gray(collection{index(i), 1}), fspecial("sobel"));%collection{index(i), 1}, fspecial("laplcian"))
+    title_string = [farms{collection{index(i), 2}.farm_index},' ', cultivations{collection{index(i), 2}.cultivation_index},' ',shoots{collection{index(i), 2}.shoot_index}];
+    subplot(2, 2, i); imshow(img); title(title_string);
+end
+%}
+%
+
+for i = 1:1
+     title_string = [farms{collection{index(i), 2}.farm_index},' ', cultivations{collection{index(i), 2}.cultivation_index},' ',shoots{collection{index(i), 2}.shoot_index}];
+    img = im2double(rgb2gray(collection{index(i), 1}));
+    figure('name', 'Normal');
+    imshow(img); title(title_string);
+    
+    img = imfilter(img, fspecial('gaussian', 13, 7));
+    img_bilateral = imbilatfilt(img, 0.1, 18, 'NeighborhoodSize', 9);
+    
+    figure('name', 'Bilateral filtering'); 
+    imshow(img_bilateral); title(title_string);
+    
+    img_horizontal = abs(imfilter(img_bilateral, fspecial("sobel")));
+    img_vertical = abs(imfilter(img_bilateral, fspecial("sobel")'));
+    
+    threshold = 0.2;
+    
+    threshold_index = find(img_vertical < threshold);
+    img_vertical(threshold_index) = 0;
+      threshold_index = find(img_vertical >= threshold);
+    img_vertical(threshold_index) = 1;
+
+     threshold_index = find(img_horizontal < threshold);
+    img_horizontal(threshold_index) = 0;
+     threshold_index = find(img_horizontal >= threshold);
+    img_horizontal(threshold_index) = 1;
+    
+    
+    
+    
+    img = img_vertical + img_horizontal;
+    
+   figure('name', 'Sobel edges'); 
+   imshow(img); title(title_string);
+end
+
 
 %% 2.3 Object identification and statistisc collection
 
